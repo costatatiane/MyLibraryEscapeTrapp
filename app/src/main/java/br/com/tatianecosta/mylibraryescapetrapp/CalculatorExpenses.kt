14c269b5dd7ee7b60a17d4dde6cee7185986faa2
karlapsouza <karlapsouza@hotmail.com>
2020-09-16 12:29:30 -0300

    Update CalculatorExpenses.kt

--- app/src/main/java/br/com/tatianecosta/mylibraryescapetrapp/CalculatorExpenses.kt
+++ app/src/main/java/br/com/tatianecosta/mylibraryescapetrapp/CalculatorExpenses.kt
@@ -1,8 +1,13 @@
 package br.com.tatianecosta.mylibraryescapetrapp
 
 class CalculatorExpenses {
-    fun calculateExpenses(totalExpense: Double , value: Double): Double {
+    fun sumExpenses(totalExpense: Double , value: Double): Double {
         val total = totalExpense + value
+        return total
+    }
+    
+    fun subtractionExpenses(totalExpense: Double , value: Double): Double {
+        val total = totalExpense - value
         return total
     }
 }
